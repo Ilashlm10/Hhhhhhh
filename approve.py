02d2925b12e9f3b2bb28c85b8cde53f14d05ad80
Ilashlm10 <ilashadam3@gmail.com>
2024-07-30 01:40:00 +0400

    Create approve.py

---
+++ approve.py
@@ -0,0 +1,18 @@
+from pyrogram.errors import InputUserDeactivated, UserNotParticipant, FloodWait, UserIsBlocked, PeerIdInvalid
+from pyrogram import Client, filters
+from pyrogram.types import *
+from motor.motor_asyncio import AsyncIOMotorClient  
+from os import environ as env
+import asyncio, datetime, time
+
+
+ACCEPTED_TEXT = "Hey {user}\n\nYour Request For {chat} Is Accepted ✅"
+
+@Bot.on_chat_join_request()
+async def req_accept(c, m):
+    user_id = m.from_user.id
+    chat_id = m.chat.id
+    if not await Data.find_one({'id': user_id}): await Data.insert_one({'id': user_id})
+    await c.approve_chat_join_request(chat_id, user_id)
+    try: await c.send_message(user_id, ACCEPTED_TEXT.format(user=m.from_user.mention, chat=m.chat.title))
+    except Exception as e: print(e)
